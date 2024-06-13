a9685811347f9a0992b7e3d5bd698fb9da887e35
Fajar adhitia pratama <115777980+fajaradhitiapratama@users.noreply.github.com>
2024-06-13 22:25:49 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const mqtt = require('mqtt')
-const options = require('./setup/Hivemq.js').options
-const express = require('express');
+const mqtt = require("mqtt");
+const options = require("./setup/Hivemq.js").options;
+const express = require("express");
 
-const db = require('./setup/database.js').app
-const firebase = require('./setup/database.js').fb
+const db = require("./setup/database.js").app;
+const firebase = require("./setup/database.js").fb;
 
 const database = firebase.getDatabase(db);
 const rootRef = firebase.ref(database);
@@ -11,53 +11,54 @@ const rootRef = firebase.ref(database);
 app = express();
 var client = mqtt.connect(options);
 
-client.on('connect', function () {
-  console.log('Connected');
-  client.subscribe('esp8266_data');
-  client.on('message', (topic, message) => {
+client.on("connect", function () {
+  console.log("Connected");
+  client.subscribe("esp8266_data");
+  client.on("message", (topic, message) => {
     console.log(message.toString());
     validator(message);
   });
 });
 
-function slice_data(message){
+function slice_data(message) {
   try {
-    message = message.toString()
-  var data = []
-  data = message.split('#')
-  data.shift()
+    message = message.toString();
+    var data = [];
+    data = message.split("#");
+    data.shift();
 
-  console.log(data);
+    console.log(data);
 
-  return data;
+    return data;
   } catch (error) {
     return error;
   }
-  
 }
 
-async function validator (message) {
+async function validator(message) {
   message = message.toString();
   var data = [];
   data = slice_data(message);
 
-  if(data[0]=="AWS"){
+  if (data[0] == "AWS") {
     add_history_aws(data);
-  } else{
-  const device = await check_device (data[0])
-  if(!device){
-    add_device(data)
-    add_history(data)
-  } else{
-    add_history(data)
-  }
+  } else {
+    const device = await check_device(data[0]);
+    if (!device) {
+      add_device(data);
+      add_history(data);
+    } else {
+      add_history(data);
+    }
   }
 }
 
 check_device = (id) => {
-  return new Promise( async (resolve, reject) => {
-      try {
-        firebase.get(firebase.ref(database,'SOIL/'+id)).then((snapshot) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      firebase
+        .get(firebase.ref(database, "SOIL/" + id))
+        .then((snapshot) => {
           if (snapshot.exists()) {
             console.log("Data available");
             resolve(true);
@@ -65,120 +66,118 @@ check_device = (id) => {
             console.log("No data available");
             resolve(false);
           }
-        }).catch((error) => {
+        })
+        .catch((error) => {
           console.error("Error getting data:", error);
         });
-      } catch (error) {
-          console.log(error)
-          reject(error)
-      }
-  })
-}
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
 
 add_device = (data) => {
-
-  return new Promise( async (resolve, reject) => {
-      try {
-        firebase.set(firebase.ref(database, 'SOIL/'+data[0]),{
-          long:data[1],
-          lat:data[2],
-          mac_address:'3C:71:BF:2A:D7:9E',
-          jenis_iot : data[0]
-      }).then(() => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      firebase
+        .set(firebase.ref(database, "SOIL/" + data[0]), {
+          long: data[1],
+          lat: data[2],
+          mac_address: "3C:71:BF:2A:D7:9E",
+          jenis_iot: data[0],
+        })
+        .then(() => {
           // Data saved successfully!
-          console.log('Data saved successfully')
-          resolve(true)
+          console.log("Data saved successfully");
+          resolve(true);
         })
         .catch((error) => {
           // The write failed...
-          console.log('The write failed')
-          reject(error)
+          console.log("The write failed");
+          reject(error);
         });
-      } catch (error) {
-          console.log(error)
-          reject(error)
-      }
-  })
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
 
-}
-
-function add_history(data){
-
-  console.log('masuk');
-  const newData = [ 
-    { 
+function add_history(data) {
+  console.log("masuk");
+  const newData = [
+    {
       N: data[3],
       P: data[4],
-      K: data[5], 
-      PH: data[6], 
-      mosit: data[7], 
+      K: data[5],
+      PH: data[6],
+      mosit: data[7],
       rssi: data[8],
       timestamp_alat: data[9],
-      timestamp_server: new Date,
-     } 
-  ]
-  firebase.get(firebase.ref(database,'HISTORY/SOIL/'+data[0]))
+      timestamp_server: new Date(),
+    },
+  ];
+  firebase
+    .get(firebase.ref(database, "HISTORY/SOIL/" + data[0]))
     .then((snapshot) => {
-        if (snapshot.exists()) {
-          const existingData = snapshot.val().data;
-          // console.log(existingData)
+      if (snapshot.exists()) {
+        const existingData = snapshot.val().data;
+        // console.log(existingData)
 
-          const updatedData = [...existingData, ...newData];
-          // console.log(updatedData)
-          firebase.update(firebase.ref(database,'HISTORY/SOIL/'+data[0]), { 
-            data: updatedData 
-          }
-          );
-        } else {
-          console.log('ga ada')
-          
-          firebase.set(firebase.ref(database, 'HISTORY/SOIL/'+data[0]), { 
-            data: newData 
-          });
-        }
+        const updatedData = [...existingData, ...newData];
+        // console.log(updatedData)
+        firebase.update(firebase.ref(database, "HISTORY/SOIL/" + data[0]), {
+          data: updatedData,
+        });
+      } else {
+        console.log("ga ada");
+
+        firebase.set(firebase.ref(database, "HISTORY/SOIL/" + data[0]), {
+          data: newData,
+        });
+      }
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error);
     });
-
 }
 
-function add_history_aws(data){
-  const newData = [ 
-    { 
-      Temp : data[1],
-      Humid : data[2],
-      Wind_speed : data[3],
-      Pressure : data[4],
-      Rain : data[5],
-      Intensity : data[6],
-      Wind_dir : data[7],
-      Wind_gust : data[8],
+function add_history_aws(data) {
+  const newData = [
+    {
+      Temp: data[1],
+      Humid: data[2],
+      Wind_speed: data[3],
+      Pressure: data[4],
+      Rain: data[5],
+      Intensity: data[6],
+      Wind_dir: data[7],
+      Wind_gust: data[8],
       rssi: data[9],
       timestamp_alat: data[10],
-      timestamp_server: new Date,
-     } 
-  ]
-  firebase.get(firebase.ref(database,'HISTORY/WEATHER/'+data[0]))
+      timestamp_server: new Date(),
+    },
+  ];
+  firebase
+    .get(firebase.ref(database, "HISTORY/WEATHER/" + data[0]))
     .then((snapshot) => {
-        if (snapshot.exists()) {
-          const existingData = snapshot.val().data;
+      if (snapshot.exists()) {
+        const existingData = snapshot.val().data;
 
-          const updatedData = [...existingData, ...newData];
-          firebase.update(firebase.ref(database,'HISTORY/WEATHER/'+data[0]), { 
-            data: updatedData 
-          }
-          );
-        } else {
-          console.log('ga ada')
-          
-          firebase.set(firebase.ref(database, 'HISTORY/WEATHER/'+data[0]), { 
-            data: newData 
-          });
-        }
+        const updatedData = [...existingData, ...newData];
+        firebase.update(firebase.ref(database, "HISTORY/WEATHER/" + data[0]), {
+          data: updatedData,
+        });
+      } else {
+        console.log("ga ada");
+
+        firebase.set(firebase.ref(database, "HISTORY/WEATHER/" + data[0]), {
+          data: newData,
+        });
+      }
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error);
     });
 }
-
--- consumer.js
+++ consumer.js
@@ -1,24 +1,27 @@
-let db = require('./app').db
-let chanel = require('./app').chenel
+let db = require("./app").db;
+let chanel = require("./app").chenel;
 
-let user_collection = db.collection('A1EC19E0A2')
-let history_collection
+let user_collection = db.collection("A1EC19E0A2");
+let history_collection;
 
-function consume () {
-    console.log("connect")
-    chanel.assertExchange('amq.topic', 'topic', {
-        durable: true
-    })
-    chanel.assertQueue("pertanian", {exclusive: false}, function (err, res) {
-        if (err) {
-            console.log('Error ', err)
-        } else {
-            chanel.consume(res.queue, function (msg) {
-                validator(msg.content.toString())
-            },
-            {
-                noAck: true
-            })
+function consume() {
+  console.log("connect");
+  chanel.assertExchange("amq.topic", "topic", {
+    durable: true,
+  });
+  chanel.assertQueue("pertanian", { exclusive: false }, function (err, res) {
+    if (err) {
+      console.log("Error ", err);
+    } else {
+      chanel.consume(
+        res.queue,
+        function (msg) {
+          validator(msg.content.toString());
+        },
+        {
+          noAck: true,
         }
-    })
+      );
+    }
+  });
 }
